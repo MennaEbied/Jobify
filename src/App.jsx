3bfd6fb4fc82352cb1b320166c4972ca4310c47a
MennaEbied <mennahesham253@gmail.com>
2025-07-06 05:22:00 +0300

    Add dashboard routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,22 @@ import {
   EditJob,
   Login,
   Profile,
-  Landing
+  Landing,
 } from "./pages";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
-    errorElement: <Error/>,
+    errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Landing/>
+        element: <Landing />,
       },
       {
         path: "register",
-        element: <Register/>
+        element: <Register />,
       },
       {
         path: "login",
@@ -35,8 +35,30 @@ const router = createBrowserRouter([
       {
         path: "dashboard",
         element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+          },
+          {
+            path: "stats",
+            element: <Stats />,
+          },
+           {
+            path: "all-jobs",
+            element: <AllJobs />,
+          },
+           {
+            path: "profile",
+            element: <Profile />,
+          },
+           {
+            path: "admin",
+            element: <Admin/>,
+          },
+        ],
       },
-    ]
+    ],
   },
 ]);
 
