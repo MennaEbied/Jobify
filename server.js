a6226bfa811e54878b955bd52494930883634663
MennaEbied <mennahesham253@gmail.com>
2025-07-09 22:07:35 +0300

    Create a job

--- server.js
+++ server.js
@@ -23,9 +23,24 @@ app.post("/", (req, res) => {
   res.json({ message: "data received", data: req.body });
 });
 
+//Get All Jobs
 app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
 });
+
+//Create Job
+app.post("/api/v1/jobs", (req, res) => {
+    const{company, position} = req.body
+    if(!company || !position){
+         res.status(400).json({msg:"provide a company and a position"})
+         return
+    }
+    const id = nanoid(10)
+    const job = {id, company, position}
+    jobs.push(job)
+    res.status(200).json({ job });
+  });
+
 
 const port = process.env.PORT || 5100;
 
