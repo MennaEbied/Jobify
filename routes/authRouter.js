8a87a662d29bc51d235d74e63b37202af0be710c
MennaEbied <mennahesham253@gmail.com>
2025-07-22 04:43:44 +0300

    Create login validation middleware

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 const router = Router();
 
 import { login, register } from "../controllers/authControllers.js";
-import { validateRegisterInut } from "../middleware/validationMiddleware.js";
+import { validateRegisterInut, validateLoginInput } from "../middleware/validationMiddleware.js";
 
 
 router.post("/register",validateRegisterInut, register);
-router.post("/login", login);
+router.post('/login', validateLoginInput, login);
 
 
 export default router
