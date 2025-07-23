e450358a659ccf6725506d8bb576f0ff442ae811
MennaEbied <mennahesham253@gmail.com>
2025-07-23 05:56:12 +0300

    Set up logout controller

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 const router = Router();
 
-import { login, register } from "../controllers/authControllers.js";
-import { validateRegisterInut, validateLoginInput } from "../middleware/validationMiddleware.js";
+import { login, logout, register } from "../controllers/authControllers.js";
+import {
+  validateRegisterInut,
+  validateLoginInput,
+} from "../middleware/validationMiddleware.js";
 
+router.post("/register", validateRegisterInut, register);
+router.post("/login", validateLoginInput, login);
+router.get("/logout", logout);
 
-router.post("/register",validateRegisterInut, register);
-router.post('/login', validateLoginInput, login);
-
-
-export default router
+export default router;
