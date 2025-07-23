d285b9a6f43552474853605c8bceaa13bc9305fe
MennaEbied <mennahesham253@gmail.com>
2025-07-23 06:37:02 +0300

    Set up user routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import mongoose from 'mongoose';
 import jobRouter from './routes/jobRouter.js'
 import authRouter from './routes/authRouter.js'
+import userRouter from "./routes/userRouter.js";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
 import cookieParser from "cookie-parser";
@@ -28,6 +29,7 @@ app.post("/", (req, res) => {
 });
 
 app.use('/api/v1/jobs',authenticateUser, jobRouter)
+app.use('/api/v1/users',authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
 //Not Found
