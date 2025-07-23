068bb70e18a484c18ab420ee61b98e0e0c1637bb
MennaEbied <mennahesham253@gmail.com>
2025-07-23 04:33:07 +0300

    Set up http only cookie

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -20,5 +20,11 @@ export const login = async (req, res) => {
   const isValidUser = User && (await comparePassword(req.body.password, user.password))
   if(!isValidUser) throw new UnauthenticatedError('invalid credentials')
     const token  = createJWT({userId:user._id, role:user.role})
-    res.json({token})
+  const oneDay = 1000*60*60*24
+  res.cookie('token', token,{
+    httpOnly:true,
+    expires: new Date(Date.now()+oneDay),
+    secure:process.env.NODE_ENV === 'production'
+  })
+    res.status(StatusCodes.OK).json({msg:'user logged in '})
 };
