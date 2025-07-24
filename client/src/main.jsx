92a9af1866240b485836653cedc7f830e5e422e6
MennaEbied <mennahesham253@gmail.com>
2025-07-24 05:45:02 +0300

    React toastify set up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css'
 import App from './App.jsx'
+import { ToastContainer } from 'react-toastify';
 
 fetch('/api/v1/test')
   .then((res) => res.json())
@@ -10,5 +12,6 @@ fetch('/api/v1/test')
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <ToastContainer position='top-center' />
   </StrictMode>,
 )
