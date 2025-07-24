6678f80bef27b94c4ddb9af03d5a078212af6d6a
MennaEbied <mennahesham253@gmail.com>
2025-07-25 00:43:16 +0300

    Get current user set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import {
 } from "./pages";
 import { action as registerAction } from "./pages/Register";
 import{action as loginAction} from './pages/Login';
-
+import {loader as dashboardLoader} from './pages/DashboardLayout'
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true'
   document.body.classList.toggle('dark-theme', isDarkTheme)
@@ -45,6 +45,7 @@ const router = createBrowserRouter([
       {
         path: "dashboard",
         element: <DashboardLayout  />,
+        loader:dashboardLoader,
         children: [
           {
             index: true,
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,13 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, redirect, useLoaderData } from 'react-router-dom';
 import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
 import { createContext, useContext, useState } from "react";
 import { checkDefaultTheme } from "../App";
+import customFetch from "../utils/customFetch";
+
+export const loader = async () => {
+  try {
+    const {data} = await customFetch.get('/users/current-user')
+    return data
+  } catch (error) {
+    return redirect('/')
+  }
+}
 const DashboardContext = createContext();
 
 const DashboardLayout = () => {
+  const {user} = useLoaderData()
 
-  const user = { name: "John" };
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setDarkTheme] = useState(checkDefaultTheme);
 
@@ -43,7 +53,7 @@ const DashboardLayout = () => {
           <div>
             <Navbar />
             <div className="dashboard-page">
-              <Outlet />
+              <Outlet context={{user}}/>
             </div>
           </div>
         </main>
