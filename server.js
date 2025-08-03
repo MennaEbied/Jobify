c66531e677181ca221c1fb4582323e19a18bce10
MennaEbied <mennahesham253@gmail.com>
2025-08-03 06:45:19 +0300

    Public folder

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ const app = express();
 
 import morgan from "morgan";
 import mongoose from 'mongoose';
+import cookieParser from "cookie-parser";
+
+//ROUTERS
 import jobRouter from './routes/jobRouter.js'
 import authRouter from './routes/authRouter.js'
 import userRouter from "./routes/userRouter.js";
+
+//PUBLIC
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+//MIDDLEWARE
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
-import cookieParser from "cookie-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, './public')));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
