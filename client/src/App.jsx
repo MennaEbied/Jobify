2b3dac133435c5b13b41256f723b2488142c5286
MennaEbied <mennahesham253@gmail.com>
2025-08-03 05:42:28 +0300

    Admin page setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ import {loader as allJobsLoader} from './pages/AllJobs'
 import { loader as editJobLoader } from './pages/EditJob';
 import { action as editJobAction } from './pages/EditJob';
 import { action as deleteJobAction } from './pages/DeleteJob';
-
+import {loader as adminLoader} from './pages/Admin'
 
 
 
@@ -78,6 +78,7 @@ const router = createBrowserRouter([
            {
             path: "admin",
             element: <Admin/>,
+            loader: adminLoader
           },
           {
             path: 'edit-job/:id',
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable no-unused-vars */
+import { FaSuitcaseRolling, FaCalendarCheck } from 'react-icons/fa';
+import { useLoaderData, redirect } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import { toast } from 'react-toastify';
+
+export const loader =async () => {
+  try {
+    const response = await customFetch.get('/users/admin/app-stats')
+    return response.data
+  } catch (error) {
+    toast.error('You are not authorized to view this page');
+    return redirect('/dashboard');
+  }
+}
 
 const Admin = () => {
+  const {users, jobs} = useLoaderData()
   return (
-    <h1>Admin</h1>
+    <Wrapper>
+      <h1>
+        Admin page
+      </h1>
+      </Wrapper>
   )
 }
 
