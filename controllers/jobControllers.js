b85497aaabf06877ec4860e5c5f4a8e649c264c5
MennaEbied <mennahesham253@gmail.com>
2025-08-06 22:22:35 +0300

    Group by job stats

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -2,15 +2,15 @@ import "express-async-errors";
 import Job from "../models/jobModel.js";
 import { StatusCodes } from "http-status-codes";
 import mongoose from "mongoose";
-import day from 'dayjs'
+import day from "dayjs";
 
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({createdBy: req.user.userId});
+  const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
-  req.body.createdBy = req.user.userId
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
@@ -32,24 +32,36 @@ export const deleteJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "job deleted", job: removedJob });
 };
 
-export const showStats = async (req,res) => {
+export const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$jobStatus", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+  console.log(stats);
+
   const defaultStats = {
-    pending: 22,
-    interview: 11,
-    declined: 4,
+    pending: stats.pending || 0,
+    interview:stats.interview || 0,
+    declined: stats.declined || 0,
   };
 
   let monthlyApplications = [
     {
-      date: 'May 23',
+      date: "May 23",
       count: 12,
     },
     {
-      date: 'Jun 23',
+      date: "Jun 23",
       count: 9,
     },
     {
-      date: 'Jul 23',
+      date: "Jul 23",
       count: 3,
     },
   ];
