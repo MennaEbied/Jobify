eaa496f6ab667a1b8bcdd64cf6707635a2e1a618
MennaEbied <mennahesham253@gmail.com>
2025-08-10 05:21:26 +0300

    Build setup

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ cloudinary.config({
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -52,9 +52,10 @@ app.use('/api/v1/jobs',authenticateUser, jobRouter)
 app.use('/api/v1/users',authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
-app.get('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'./public','index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
+
 //Not Found
 app.use('*', (req, res) => {
   res.status(404).json({msg: "Not Found"})
@@ -64,12 +65,11 @@ app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5100;
 
 try {
-  await mongoose.connect(process.env.MONGO_URL)
-  app.listen(5100, (req, res) => {
-    console.log(`server running on port ${port}...`);
-  });
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log('DB Connected');
 } catch (error) {
-  console.log(error)
-  process.exit(1)
+  console.log(error);
+  process.exit(1);
 }
 
+export default app;
