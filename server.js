3eba040fda2c5a4757e4b502aaafa2b908c57bac
MennaEbied <mennahesham253@gmail.com>
2025-08-10 06:24:28 +0300

    Cancel deployment

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ cloudinary.config({
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -51,8 +52,9 @@ app.use('/api/v1/jobs',authenticateUser, jobRouter)
 app.use('/api/v1/users',authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
-
-
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
 //Not Found
 app.use('*', (req, res) => {
   res.status(404).json({msg: "Not Found"})
@@ -62,11 +64,12 @@ app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5100;
 
 try {
-  await mongoose.connect(process.env.MONGO_URL);
-  console.log('DB Connected');
+  await mongoose.connect(process.env.MONGO_URL)
+  app.listen(5100, (req, res) => {
+    console.log(`server running on port ${port}...`);
+  });
 } catch (error) {
-  console.log(error);
-  process.exit(1);
+  console.log(error)
+  process.exit(1)
 }
 
-export default app;
